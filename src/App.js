175b93eed3c57a1ad4aca0ff1cca172a596a8c0b
vinisiq <viniciusdp2@outlook.com>
2022-10-05 12:10:17 -0300

    ainda testando

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, Deivison!.
+          Hello, Vinicius!
         </p>
         <a
           className="App-link"
