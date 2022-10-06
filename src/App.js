c49f5fcf8aa37123c35bbe81daab4b60be72ce5a
vinisiq <viniciusdp2@outlook.com>
2022-10-06 13:11:48 -0300

    subindo alterações

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Vamos ver se esse teste funciona
+          Vinicius
         </p>
         <a
           className="App-link"
